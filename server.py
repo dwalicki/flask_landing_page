5affdac86fd1ca7ed0847cfab0a725d131b40e7d
Daniel Walicki <danielrwalicki@gmail.com>
2018-09-06 21:49:58 -0700

    Flask the Landing Page

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     return render_template("index.html")
+
+@app.route('/dojos')
+def dojos():
+    return render_template("dojos.html")
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template("ninjas.html")
+
+app.run(debug=True)
+
+
